9736f9166c371df2357fca02a2cba2efa0163677
oliviaamiller <oliviaoliviaolivia@gmail.com>
2021-11-30 11:07:26 -0800

    tested and wrote third funciton

--- functions.js
+++ functions.js
@@ -46,7 +46,7 @@ Output:
 */
 
 export function multiplyBy12ThenHalve(num) {
-    return true;
+    return (num * 12) / 2;
 }
 
 
--- test/functions.test.js
+++ test/functions.test.js
@@ -2,6 +2,7 @@
 import { 
     addExclamationPoints,
     multiplyBySeven,
+    multiplyBy12ThenHalve,
 } from '../functions.js';
 
 const { test, skip } = QUnit;
@@ -22,7 +23,7 @@ skip('addExclamationPoints should return a string with 3 exclamation points adde
 });
 
 
-test('multiplyBySeven should take a number and multiply it by 7', (expect) => {
+skip('multiplyBySeven should take a number and multiply it by 7', (expect) => {
     const expected1 = 14;
     const actual1 = multiplyBySeven(2);
 
@@ -35,5 +36,20 @@ test('multiplyBySeven should take a number and multiply it by 7', (expect) => {
     expect.equal(actual1, expected1, '2 * 7 = 14');
     expect.equal(actual2, expected2, '7 * 7 = 49');
     expect.equal(actual3, expected3, '5.5 * 7 = 38.5');
+});
+
+test('multiplyBy12ThenHalve should take a number, multiply it by 12, and then halve that number', (expect) => {
+    const expected1 = 12;
+    const actual1 = multiplyBy12ThenHalve(2);
+
+    const expected2 = 42;
+    const actual2 = multiplyBy12ThenHalve(7);
+
+    const expected3 = 33;
+    const actual3 = multiplyBy12ThenHalve(5.5);
+
+    expect.equal(actual1, expected1, '(2 * 12) / 2 = 12');
+    expect.equal(actual2, expected2, '(7 * 12) / 2 = 42');
+    expect.equal(actual3, expected3, '(5.5 * 12) / 2 = 33');
 });
 
